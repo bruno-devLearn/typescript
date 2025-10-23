f36a8344f1d86e68934fa51bd2c616613c2df021
bruno-devlearn <qbruno570@gmail.com>
2025-10-23 17:37:10 -0300

    input design

--- aula007/weatherApp/src/App.tsx
+++ aula007/weatherApp/src/App.tsx
@@ -1,6 +1,7 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "./App.css";
 import { Header } from "./Components/Header";
+import { Outlet } from "react-router";
 
 const queryClient = new QueryClient();
 
@@ -9,7 +10,9 @@ function App() {
         // Provide the client to your App
         <QueryClientProvider client={queryClient}>
             <Header />
-            {/* Compoents */}
+            <main className="search">
+                <Outlet />
+            </main>
         </QueryClientProvider>
     );
 }
--- aula007/weatherApp/src/main.tsx
+++ aula007/weatherApp/src/main.tsx
@@ -1,12 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router";
+import { BrowserRouter, Route, Routes } from "react-router";
 import App from "./App";
+import { Input } from "./Components/Routers/Search/Input";
 
 const root = document.getElementById("root");
 
 ReactDOM.createRoot(root).render(
     <BrowserRouter>
-        <App />
+        <Routes>
+            <Route path="/" element={<App />}>
+                <Route index element={<Input />} />
+            </Route>
+        </Routes>
     </BrowserRouter>
 );
