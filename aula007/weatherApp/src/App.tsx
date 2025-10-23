8e411167bf4fa04ed9f13d5403ca5bd8e9a7318e
bruno-devlearn <qbruno570@gmail.com>
2025-10-23 17:03:01 -0300

    header design + link

--- aula007/weatherApp/src/App.tsx
+++ aula007/weatherApp/src/App.tsx
@@ -1,5 +1,6 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "./App.css";
+import { Header } from "./Components/Header";
 
 const queryClient = new QueryClient();
 
@@ -7,6 +8,7 @@ function App() {
     return (
         // Provide the client to your App
         <QueryClientProvider client={queryClient}>
+            <Header />
             {/* Compoents */}
         </QueryClientProvider>
     );
